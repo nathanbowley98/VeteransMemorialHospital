87581ff6d3e5e8dd4fc37754beb5ef2ae5526f03
Nathanael Bowley <bowley@timberlea.CS.Dal.Ca>
2021-06-06 00:08:13 -0300

    when run on timberlea now the program can now output the ns_map.html, it should work on all the volunteers computers if they want to test it

--- mapping/mapping.py
+++ mapping/mapping.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
+import sys
+print(sys.path)
+#sys.path.append('/users/webhome/bowley/volunteering/veterans_memorial/VeteransMemorialHospital/mapping/venv/Lib/site-packages')
+sys.path.append('./venv/Lib/site-packages')
+print(sys.path)
+#import venv/Lib/site-packages/folium/__init__.py
+
 import folium
 from folium.plugins import MarkerCluster
 
+print(sys.path)
 
 coords_ns = [45.24664938443739, -63.24613206184815]
 map = folium.Map(location=coords_ns, zoom_start=8)
