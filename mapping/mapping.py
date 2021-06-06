21525b0fcb7b3b99ef9b84ba1ab76b4dce4e7238
bowley <nt620579@dal.ca>
2021-06-05 23:32:08 -0300

    updated the file to test to see if html file is produced

--- mapping/mapping.py
+++ mapping/mapping.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 
+import folium
+from folium.plugins import MarkerCluster
 
-print("testing")
+
+coords_ns = [45.24664938443739, -63.24613206184815]
+map = folium.Map(location=coords_ns, zoom_start=8)
+map.save('ns_map.html')
