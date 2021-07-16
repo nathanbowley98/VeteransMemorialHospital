057f06d9d45804221300e2d4c37d7bbdd7aa9322
Nathanael Bowley <bowley@timberlea.CS.Dal.Ca>
2021-07-16 19:19:14 -0300

    updated the mapping.py to fetch the queries from the database and it is returning the values correctly, however theres a file outside of the source code that is not included which has database information so I cant include that

--- mapping/mapping.py
+++ mapping/mapping.py
@@ -1,17 +1,44 @@
 #!/usr/bin/env python
 
 import sys
-print(sys.path)
-#sys.path.append('/users/webhome/bowley/volunteering/veterans_memorial/VeteransMemorialHospital/mapping/venv/Lib/site-packages')
 sys.path.append('./venv/Lib/site-packages')
-print(sys.path)
-#import venv/Lib/site-packages/folium/__init__.py
 
+#mapping imports
 import folium
 from folium.plugins import MarkerCluster
 
-print(sys.path)
+#source code is public but the database password must be kept secret sorry.
+sys.path.append('../../databaseSecret')
+from database import config
+
+#reading from mysql server thats hosted on the dal servers
+import mysql.connector
+connection = mysql.connector.connect(**config)
+
+cursor = connection.cursor()
+query = ("SELECT * FROM VetMSingleTable")
+cursor.execute(query)
+result = cursor.fetchall()
+for row in result:
+    print(row)
+
+#connection = pyodbc.connect('Driver={};'
 
 coords_ns = [45.24664938443739, -63.24613206184815]
 map = folium.Map(location=coords_ns, zoom_start=8)
+
+latitude = 45.24
+longitude = -63.246
+
+tuple = (latitude, longitude)
+tuple2 = (45.30, -63.25)
+
+points = [tuple, tuple2]
+
+folium.PolyLine(points).add_to(map)
+
+
+
 map.save('ns_map.html')
+
+
